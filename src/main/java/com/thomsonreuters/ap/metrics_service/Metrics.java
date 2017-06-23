8e212282b3ed2858412bf755bd25c649dd5c42eb
Xuefeng <Xuefeng.Chang@thomsonreuters.com>
2017-06-23 11:15:50 -0400

    remove generated files

--- src/main/java/com/thomsonreuters/ap/metrics_service/Metrics.java
+++ src/main/java/com/thomsonreuters/ap/metrics_service/Metrics.java
@@ -12,18 +12,17 @@ import java.net.Socket;
 public class Metrics {
   private static final String host = "10.194.232.101";
   private static final int port = 2003;
-  
-  private Metrics() {
-  }
-  
+
+  private Metrics() {}
+
   public static void send(String metricPath, String metricsValue, long timestamp) {
     try {
       Socket socket = new Socket(host, port);
       System.err.println("Connecting to " + host + " port " + port);
-      
+
       OutputStream output = socket.getOutputStream();
       PrintWriter writer = new PrintWriter(output);
-      String data = String.format("%s %s %d", metricPath, metricsValue, timestamp/1000);
+      String data = String.format("%s %s %d", metricPath, metricsValue, timestamp / 1000);
       writer.println(data);
       writer.flush();
       System.out.println(data);
@@ -31,11 +30,12 @@ public class Metrics {
     } catch (IOException e) {
       System.err.println("send metrics data exception" + e);
     }
-}
+  }
 
   public static void main(String[] args) {
-    for(int i=0; i< 10; ++i) {
-      Metrics.send("test.java.metrics", String.valueOf(100+i), System.currentTimeMillis() - (i * 60 * 60 * 1000));
+    for (int i = 0; i < 10; ++i) {
+      Metrics.send("test.java.metrics", String.valueOf(100 + i),
+          System.currentTimeMillis() - (i * 60 * 60 * 1000));
     }
   }
 }
