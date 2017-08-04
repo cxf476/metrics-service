65e389f0404de830b6c8c00374c908b79666d586
Xuefeng <Xuefeng.Chang@thomsonreuters.com>
2017-08-04 10:30:40 -0400

    switch to none-prod metrics server

--- src/main/java/com/thomsonreuters/ap/metrics/SocketMetrics.java
+++ src/main/java/com/thomsonreuters/ap/metrics/SocketMetrics.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class SocketMetrics implements Metrics {
-  public static final String DEFAULT_HOST = "34.196.109.99";
+  public static final String DEFAULT_HOST = "54.165.38.59";
   public static final int DEFAULT_PORT = 2003;
   public static final int TIMEOUT = 2000;//ms
   
